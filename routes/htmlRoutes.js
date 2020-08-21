35171a704afa15256134ba8a9af0d5fb595bb547
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-20 20:58:35 -0400

    added routes dir, route files, set up export in htmlRoutes, added server file with express config, routers, listener

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+// GET /notes - Should return the notes.html file.
+// GET * - Should return the index.html file
+
+const path = require("path");
+
+module.exports = function(app) {
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+        
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+}
