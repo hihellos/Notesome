ab07aa9cd46e42b91a75d2621f30ef1aa587afe8
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-21 14:00:07 -0400

    fixed routing

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 // Express config
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Router
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Listener
